0a084faea825d6083f695cdaf5ab6cea3fe35292
Edgar Akopyan <47034548+edgarakopyan@users.noreply.github.com>
2021-07-20 12:17:44 +0200

    Update Random_Matrix_Theory.py

--- Random_Matrix_Theory.py
+++ Random_Matrix_Theory.py
@@ -2,6 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+n = 100
+A = np.random.randn(n,n)
+GOE = (A+A.T)/np.sqrt(2*n)
+
+eig = np.linalg.eigvals(GOE)
+plt.hist(GOE)
+
+# But as we increase dimenstion we get a clearer picture of 
+# semicircle
+
+n = 1000
 A = np.random.randn(n,n)
 GOE = (A+A.T)/np.sqrt(2*n)
 
